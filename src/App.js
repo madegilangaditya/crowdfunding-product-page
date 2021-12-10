40dab647600ede3a261a351ebe641e32f6da3629
gilang aditya <madegilangaditya32@gmail.com>
2021-12-10 16:17:27 +0800

    add style and create menu popup using use state

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./components/Home";
 function App() {
   return (
     <div className="App">
+      <div className='bg-overlay'></div>
       <Header/>
       <Home/>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../images/logo.svg'
 import hamburger from '../images/icon-hamburger.svg'
+import closeMenu from '../images/icon-close-menu.svg'
 
 const Header = () => {
+    const [openMenu, setOpenMenu] = useState(false)
+
+    const menuClicked = ()=>{
+        const menuWrap = document.querySelector('.main-navigation-wrap')
+        const bgWrap = document.querySelector('.bg-overlay')
+        
+        bgWrap.classList.toggle('active')
+        menuWrap.classList.toggle('active')
+        setOpenMenu(!openMenu)
+    }
+
     return (
         <section className="header banner">
             <header className="site-header">
                 <div className="logo-wrapper">
                     <img src={logo} alt="Logo" />
                 </div>
+                <div className="mobile-menu-icon">
+                    <img className="icon-menu" src={!openMenu ? hamburger : closeMenu} alt="Icon menu" onClick={menuClicked} />
+                </div>
                 <div className="main-navigation-wrap">
                     <nav className="main-navigation">
-                        <img src={hamburger} alt="Icon menu" />
                         <ul className="main-menu">
                             <li className="menu-item">About</li>
                             <li className="menu-item">Discover</li>
